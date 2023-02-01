16474d7318a68153ed7c3a0cab7a89c52549b113
abduarrahman <abdu.arrahman@bukitmakmur.com>
2023-02-01 13:41:10 +0700

    Initial commit

---
+++ app/src/main/java/id/teknografer/hackerranksolutionkotlin/problem_solving/simple_array_sum.kt
@@ -0,0 +1,24 @@
+/**
+ * Created by abduarrahman on 01,February,2023
+ */
+
+package id.teknografer.hackerranksolutionkotlin.problem_solving
+
+
+object SimpleArraySum {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val arCount = readLine()!!.trim().toInt()
+
+        val ar = readLine()!!.trimEnd().split(" ").map { it.toInt() }.toTypedArray()
+
+        val result = simpleArraySum(ar)
+
+        println(result)
+    }
+
+    private fun simpleArraySum(ar: Array<Int>): Int {
+        // Write your code here
+        return ar.sum();
+    }
+}
---
+++ app/src/main/java/id/teknografer/hackerranksolutionkotlin/problem_solving/solve_me_first.kt
@@ -0,0 +1,22 @@
+/**
+ * Created by abduarrahman on 01,February,2023
+ */
+package id.teknografer.hackerranksolutionkotlin.problem_solving
+
+import java.util.*
+
+object SolveMeFirst {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val sc = Scanner(System.`in`)
+        val num1 = sc.nextInt()
+        val num2 = sc.nextInt()
+        val sum = solveMeFirst(num1, num2)
+        println(sum)
+    }
+
+    private fun solveMeFirst(a: Int, b: Int): Int {
+        return (a + b)
+    }
+}
+
